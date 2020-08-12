4d77efd379bc028cb06e832f36efc92d63eec9a6
Sergio <sergioa.orozcoo@gmail.com>
2020-08-12 00:55:05 -0500

    add jquey

--- app2.js
+++ app2.js
@@ -1,4 +1,68 @@
-const $form = $("form");
-const $input = $("#txtmessage");
-const $send = $(".Submit-btn");
-const $chat = $(".chat");
+$(document).ready(function () {
+  const $form = $("form");
+  const $input = $("#txtmessage");
+  const $send = $(".Submit-btn");
+  const $chat = $(".chat");
+
+  $("form").submit((event) => {
+    alert("Submitted");
+    event.preventDefault();
+
+    const value = $input.val();
+
+    if (value !== "") {
+      const now = new Date();
+      const message = {
+        time: `${now.getHours()}:${now.getMinutes()}`,
+        content: value,
+        author: {
+          name: "Sergio Orozco",
+          image: "images/contacto3.jpg",
+        },
+      };
+      printMessage(message);
+      messages.push(message);
+
+      value = "";
+    }
+  });
+
+  const messages = [
+    {
+      time: "16:25",
+      content: "Hola,¿cómo están todo en orden?",
+      author: {
+        name: "Laura Buitrago",
+        image: "images/contacto1.jpg",
+      },
+    },
+    {
+      time: "16:28",
+      content: "Hola,¿cómo están todo en orden?",
+      author: {
+        name: "Laura Buitrago",
+        image: "images/contacto2.jpg",
+      },
+    },
+  ];
+  function printMessage(message) {
+    const { author, time, content } = message;
+
+    const template = `<li>
+        <div class="cwrapper">
+          <div>
+            <img class="profilepic" src="${author.image}" alt="C7">
+          </div>
+          <div>
+            <h4>${author.name}</h4>
+            <p class="profilehour">${time}</p>
+            <p class="profilemessage">${content}</p>
+          </div>
+      </li>`;
+    $chat.html() += template;
+  }
+
+  messages.forEach((message) => {
+    printMessage(message);
+  });
+});
