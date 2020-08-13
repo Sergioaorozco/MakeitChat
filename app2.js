14a27680592a3955a579d0f84fe396235f5ce7f1
Sergio <sergioa.orozcoo@gmail.com>
2020-08-13 13:29:51 -0500

    add changes

--- app2.js
+++ app2.js
@@ -1,66 +1,71 @@
-$(document).ready(function () {
-  const $form = $("form");
-  const $input = $("#txtmessage");
-  //   const $send = $(".Submit-btn");
-  const $chat = $(".chat");
+//$(document).ready(function () {
+const $form = $("form");
+const $input = $("#txtmessage");
+const $chat = $(".chat");
 
-  $form.submit((printMessage) => {
-    printMessage.preventDefault();
-    const value = $input.val();
+$form.on("submit", (event) => {
+  event.preventDefault();
+  var value = $input.val();
+  console.log(value);
 
-    if (value !== "") {
-      const now = new Date();
-      const message = {
-        time: `${now.getHours()}:${now.getMinutes()}`,
-        content: value,
-        author: {
-          name: "Sergio Orozco",
-          image: "images/contacto3.jpg",
-        },
-      };
-      printMessage(message);
-      messages.push(message);
-
-      value = "";
-    }
-  });
-
-  const messages = [
-    {
-      time: "16:25",
-      content: "Hola,¿cómo están todo en orden?",
+  if (value !== "") {
+    const now = new Date();
+    const message = {
+      time: `${now.getHours()}:${now.getMinutes()}`,
+      content: value,
       author: {
-        name: "Laura Buitrago",
-        image: "images/contacto1.jpg",
+        name: "Sergio Orozco",
+        image: "images/contacto3.jpg",
       },
-    },
-    {
-      time: "16:28",
-      content: "Hola,¿cómo están todo en orden?",
-      author: {
-        name: "Laura Buitrago",
-        image: "images/contacto2.jpg",
-      },
-    },
-  ];
-  function printMessage(message) {
-    const { author, time, content } = message;
-
-    const template = `<li>
-        <div class="cwrapper">
-          <div>
-            <img class="profilepic" src="${author.image}" alt="C7">
-          </div>
-          <div>
-            <h4>${author.name}</h4>
-            <p class="profilehour">${time}</p>
-            <p class="profilemessage">${content}</p>
-          </div>
-      </li>`;
-    $chat.html(template);
-  }
-
-  messages.forEach((message) => {
+    };
     printMessage(message);
-  });
+    messages.push(message);
+
+    $input.val("");
+  }
 });
+
+const messages = [
+  {
+    time: "16:25",
+    content: "Hola,¿cómo están todo en orden?",
+    author: {
+      name: "Laura Buitrago",
+      image: "images/contacto1.jpg",
+    },
+  },
+  {
+    time: "16:28",
+    content: "Hola,¿cómo están todo en orden?",
+    author: {
+      name: "Carlos Andrés",
+      image: "images/contacto2.jpg",
+    },
+  },
+];
+
+function printMessage(message) {
+  const { author, time, content } = message;
+
+  const template = `<li>
+  <div class="cwrapper">
+    <div>
+      <img class="profilepic" src="${author.image}" alt="C7">
+    </div>
+    <div>
+      <h4>${author.name}</h4>
+      <p class="profilehour">${time}</p>
+      <p class="profilemessage">${content}</p>
+    </div>
+</li>`;
+  $chat.html(template);
+  //$chat.html(template);
+}
+
+$.each(messages, (message) => {
+  printMessage(message);
+});
+
+//messages.forEach((message) => {
+//printMessage(message);
+//});
